f95e5a621797c46ad8a29282660f088441a491e1
Matheus Felipe Brandão da Silva <77033014+MatheusBrandao525@users.noreply.github.com>
2022-07-21 09:28:04 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:lojavirtual/models/user_manger.dart';
 import 'package:lojavirtual/screens/base/base_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Corrigindo o bug da tela vermelha ao iniciar o App 21/07/2022.
+  await Firibase.initializeApp();
   runApp(MyApp());
 }
 
